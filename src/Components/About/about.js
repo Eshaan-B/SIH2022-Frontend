44f85630b51b29817e2b233ff390030d3428ab96
marzuk <you@example.com>
2022-03-18 09:58:42 +0530

    abt page updated

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -81,7 +81,6 @@ function About() {
                 <ul className="nav nav-pills mb-3">
                   <li><a className="nav-link active" data-bs-toggle="pill" href="#tab1">About us</a></li>
                   <li><a className="nav-link" data-bs-toggle="pill" href="#tab2">Games</a></li>
-                  <li><a className="nav-link" data-bs-toggle="pill" href="#tab3">Pledge</a></li>
                 </ul>{/* End Tabs */}
                 {/* Tab Content */}
                 <div className="tab-content">
@@ -147,41 +146,20 @@ function About() {
 
                   </div>{/* End Tab 1 Content */}
                   <div className="tab-pane fade show" id="tab2">
-                    <p className="fst-italic">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
                     <div className="d-flex align-items-center mt-4">
                       <i className="bi bi-check2" />
-                      <h4>Repudianda rerum velit modi et officia quasi facilis</h4>
+                      <h4><a href="https://universitygames.kheloindia.gov.in/">1st Khelo India University Games, Odisha - 2020</a></h4>
                     </div>
-                    <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
                     <div className="d-flex align-items-center mt-4">
                       <i className="bi bi-check2" />
-                      <h4>Incidunt non veritatis illum ea ut nisi</h4>
+                      <h4><a href="https://youthgames.kheloindia.gov.in/">Khelo India Youth Games, Pune, Maharashtra</a></h4>
                     </div>
-                    <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
                     <div className="d-flex align-items-center mt-4">
                       <i className="bi bi-check2" />
-                      <h4>Omnis ab quia nemo dignissimos rem eum quos..</h4>
+                      <h4><a href="https://schoolgames.kheloindia.gov.in/">Khelo India School Games, Delhi-2018</a></h4>
                     </div>
-                    <p>Eius alias aut cupiditate. Dolor voluptates animi ut blanditiis quos nam. Magnam officia aut ut alias quo explicabo ullam esse. Sunt magnam et dolorem eaque magnam odit enim quaerat. Vero error error voluptatem eum.</p>
                   </div>{/* End Tab 2 Content */}
-                  <div className="tab-pane fade show" id="tab3">
-                    <p className="fst-italic">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
-                    <div className="d-flex align-items-center mt-4">
-                      <i className="bi bi-check2" />
-                      <h4>Repudiandae rerum velit modi et officia quasi facilis</h4>
-                    </div>
-                    <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
-                    <div className="d-flex align-items-center mt-4">
-                      <i className="bi bi-check2" />
-                      <h4>Incidunt non veritatis illum ea ut nisi</h4>
-                    </div>
-                    <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
-                    <div className="d-flex align-items-center mt-4">
-                      <i className="bi bi-check2" />
-                      <h4>Omnis ab quia nemo dignissimos rem eum quos..</h4>
-                    </div>
-                    <p>Eius alias aut cupiditate. Dolor voluptates animi ut blanditiis quos nam. Magnam officia aut ut alias quo explicabo ullam esse. Sunt magnam et dolorem eaque magnam odit enim quaerat. Vero error error voluptatem eum.</p>
-                  </div>{/* End Tab 3 Content */}
+
                 </div>
               </div>
             </div>
