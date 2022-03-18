5c65b359e17983b0efc85b7e3e81d8d6186e8689
Gautam J <gautam.jayapal@gmail.com>
2022-03-18 10:24:34 +0530

    fix bug, login route

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 import "animate.css";
 function Login() {
   const navigate = Navigate;
